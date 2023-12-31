677bbdd730a2770f2054d19b293ce0a1bcd007d6
mohammedalomari <mohammedalomari000@gmail.com>
2023-12-31 16:46:50 +0300

    adding DAO layer

---
+++ src/main/java/com/LoginAndRegister/DAO/UserRepository.java
@@ -0,0 +1,10 @@
+package com.LoginAndRegister.DAO;
+
+import com.LoginAndRegister.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
